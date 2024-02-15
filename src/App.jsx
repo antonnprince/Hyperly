8ce4c6e94fa260424909af6154bca43d2db0d8f8
antonnprince <antonprince95@gmail.com>
2024-02-15 08:26:35 +0530

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="">
+        <Nav />
     </div>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='bg-gray-900 w-56 pt-8'>
+    <h1 className='text-3xl text-white font-bold m-12'>Hyperly</h1>
+      <li className='text-xl text-white  font-semibold space-y-8 h-screen flex flex-col items-center'>
+        <ul className='hover:text-gray-500 delay-150'><a>Dashboard</a></ul>
+        <ul  className='hover:text-gray-500 delay-150'><a> Create</a></ul>
+        <ul  className='hover:text-gray-500 delay-150'><a>Settings</a></ul>
+      </li>
+    </div>
+  )
+}
+
+export default Nav
