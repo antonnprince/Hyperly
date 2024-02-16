2afc305d85ec970c3ba765965d0e953163291002
antonnprince <antonprince95@gmail.com>
2024-02-16 09:54:08 +0530

    open ai API working

--- src/button.jsx
+++ src/button.jsx
@@ -7,7 +7,7 @@ const Button = () => {
     <div>
       <button 
       onClick={()=>main()}
-      class="cursor-pointer transition-all bg-blue-500 text-white font-semibold px-6 py-2 rounded-lg
+      className="cursor-pointer transition-all bg-blue-500 text-white font-semibold px-6 py-2 rounded-lg
 border-blue-600
 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
--- src/openai.jsx
+++ src/openai.jsx
@@ -1,17 +1,14 @@
-import { OpenAI } from "openai";
+import OpenAI from "openai";
 
-const apikey = process.env.API_KEY;
-const openai = new OpenAI({ apiKey: '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',dangerouslyAllowBrowser: true  });
+const openai = new OpenAI({apiKey: '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',dangerouslyAllowBrowser:true});
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{ role: "user", content: "What is 1+1" }],
     model: "gpt-3.5-turbo",
   });
 
   console.log(completion.choices[0]);
 }
 
-main();
-
-export { main };
+export {main}
