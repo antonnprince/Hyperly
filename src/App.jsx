81d5d4c213018803db9165cb0594bae3bc9465de
antonnprince <antonprince95@gmail.com>
2024-02-18 12:48:04 +0530

    routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './Nav';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Create from './Create';
+import Nav from './Nav';
 import Dash from './Dash';
+
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-row bg-slate-200">
-        <Nav />
-        <Switch>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/dashboard">
-            <Dash />
-          </Route>
-          {/* Default route */}
-          <Route path="/">
-            <Create />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="flex flex-row bg-slate-200">
+    <BrowserRouter>
+      <Routes>
+            <Route path='/' element={<Nav />}> 
+            <Route index element={<Dash/>} />
+            <Route path="/create" element={<Create/>} />
+            </Route>
+          
+      </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import logo from './h2.png';
+import { Outlet, Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className='bg-gray-800 w-1/6 pt-8 fixed'>
+    <>
+    <nav className='bg-gray-800 w-1/6 pt-8 fixed'>
     <img src={logo} className='h-42 w-56 mx-auto' alt='Logo' />
       <li className='text-2xl text-white text-left font-thin space-y-4 h-screen flex flex-col items-center'>
-        <ul className='hover:text-gray-400 delay-150'><a>Dashboard</a></ul>
-        <ul  className='hover:text-gray-400 delay-150'><a> Create</a></ul>
-        <ul  className='hover:text-gray-400 delay-150'><a>Settings</a></ul>
+        <ul className='hover:text-gray-400 delay-150'> <Link to="/"> Dashboard </Link>  </ul>
+        <ul  className='hover:text-gray-400 delay-150'> <Link to="/create"> Create </Link></ul>
+        <ul  className='hover:text-gray-400 delay-150'>Settings</ul>
       </li>
-    </div>
+      
+    </nav>
+
+    <Outlet />
+    </>
   )
 }
 
